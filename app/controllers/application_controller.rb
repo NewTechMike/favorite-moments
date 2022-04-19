893f34e036a3d90c8f3107c19fa7fea0f9d618e6
Michael Harris <xero5k@yahoo.com>
2022-04-19 15:14:44 -0500

    Added User login/logout Backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,10 @@ class ApplicationController < ActionController::API
     session[:count] = (session[:count] || 0) + 1
     render json: { count: session[:count] }
   end
+
+
+  def authorize
+    user = User.find_by(id: session[:user_id])
+    return render json: { error: "Not Authorized" }, status: :unauthorized unless session.include? :user_id
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  get '/me', to: "users#show"
+  post '/signup', to: "users#create"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+  
+  
   get '/hello', to: 'application#hello_world'
-
   get '*path',
     to: 'fallback#index',
     constraints: ->(req) { !req.xhr? && req.format.html? }
+    
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
